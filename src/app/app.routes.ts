a7b898f77d1058f23f348f0773541304c44b2712
CHAN Suvannet <chansuvannet999@gmail.com>
2025-04-12 09:13:04 +0700

    prepare route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 // ===========================================================================>> Core Library
 import { Routes } from '@nestjs/core';
 import { AuthModule } from './resources/r1-auth/auth.module';
+import { clientRoutes } from './resources/r2-client/route';
+import { adminRoutes } from './resources/r3-admin/route';
+import { vendorRoutes } from './resources/r4-vendor/route';
+import { ProfileModule } from './resources/r5-profile/module';
 
 // ===========================================================================>> Custom Library
 export const appRoutes: Routes = [
@@ -13,16 +17,20 @@ export const appRoutes: Routes = [
       },
       {
         path: 'client',
-        // children: adminRoutes
+        children: clientRoutes
       },
       {
         path: 'admin',
-        // children: adminRoutes
+        children: adminRoutes
       },
       {
         path: 'vendor',
-        // children: adminRoutes
+        children: vendorRoutes
       },
+      {
+        path: 'profile',
+        module: ProfileModule,
+      }
     ],
   },
 ];
