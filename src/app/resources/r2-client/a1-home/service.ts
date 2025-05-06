272e7a6f77ae82d3cf338ade988eba717112f707
CHAN Suvannet <chansuvannet999@gmail.com>
2025-05-06 12:19:28 +0700

    fix errro

--- src/app/resources/r2-client/a1-home/service.ts
+++ src/app/resources/r2-client/a1-home/service.ts
@@ -1,5 +1,5 @@
 // ===========================================================================>> Core Library
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { FileService } from 'src/app/services/file.service';
 
 // ===========================================================================>> Third Party Library
@@ -8,19 +8,19 @@ import { FileService } from 'src/app/services/file.service';
 @Injectable()
 export class HomeService {
 
-    constructor(private readonly fileService: FileService) { };
-    // async create(): Promise<any> {
-       
-    //     const result = await this.fileService.uploadBase64Image('product', body.image);
-    //     if (result.error) {
-    //         throw new BadRequestException(result.error);
-    //     }
-    //     // Replace base64 string by file URI from FileService
-    //     body.image = result.file.uri;
+    constructor(private fileService: FileService) { };
+    
+    async create(body: any): Promise<any> {
+        const result = await this.fileService.uploadBase64Image('product', body.image);
+        if (result.error) {
+            throw new BadRequestException(result.error);
+        }
+        // Replace base64 string by file URI from FileService
+        body.image = result.file.uri;
 
         
-    //     return {
-    //         message: 'Product has been created.'
-    //     };
-    // }
+        return {
+            message: 'Product has been created.'
+        };
+    }
 }
