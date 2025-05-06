14384ee271ff5764d5be57bf61005f7196e5a4ea
CHAN Suvannet <chansuvannet999@gmail.com>
2025-05-06 11:49:28 +0700

    add service call to file servic

--- src/app/resources/r2-client/a1-home/service.ts
+++ src/app/resources/r2-client/a1-home/service.ts
@@ -1,8 +1,26 @@
 // ===========================================================================>> Core Library
 import { Injectable } from '@nestjs/common';
+import { FileService } from 'src/app/services/file.service';
 
 // ===========================================================================>> Third Party Library
 
 // ===========================================================================>> Custom Library
 @Injectable()
-export class HomeService {}
+export class HomeService {
+
+    constructor(private readonly fileService: FileService) { };
+    // async create(): Promise<any> {
+       
+    //     const result = await this.fileService.uploadBase64Image('product', body.image);
+    //     if (result.error) {
+    //         throw new BadRequestException(result.error);
+    //     }
+    //     // Replace base64 string by file URI from FileService
+    //     body.image = result.file.uri;
+
+        
+    //     return {
+    //         message: 'Product has been created.'
+    //     };
+    // }
+}
